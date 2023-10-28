29697f396884e8ff01b2f42e935c44381205da3c
Nils Friess <nils.friess@gmail.com>
2023-10-28 22:41:51 +0200

    Add library to parse JSON option file

--- examples/red_black_gibbs/main.cc
+++ examples/red_black_gibbs/main.cc
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -20,16 +21,22 @@
 
 using namespace pargibbs;
 
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 int main(int argc, char *argv[]) {
   mpi_helper helper(&argc, &argv);
 
-  pcg64 engine(0, mpi_helper::get_rank());
-  if (argc > 1)
-    engine.seed(std::atoi(argv[1]));
-  else {
-    pcg_extras::seed_seq_from<std::random_device> seed_source;
-    engine.seed(seed_source);
+  if (argc < 2) {
+    std::cout << "Provide path to config file as command line argument\n";
+    return 1;
   }
+  std::ifstream f(argv[1]);
+  json config = json::parse(f);
+
+  pcg_extras::seed_seq_from<std::random_device> seed_source;
+  pcg32 engine(0, mpi_helper::get_rank());
+  engine.seed(seed_source);
 
 #ifdef USE_METIS
   Lattice2D lattice(11, ParallelLayout::METIS);
@@ -40,7 +47,7 @@ int main(int argc, char *argv[]) {
   GMRFOperator prec_op(lattice);
   auto *prec_matrix = &(prec_op.matrix);
 
-  GibbsSampler sampler(&lattice, prec_matrix, &engine, true, 1.98);
+  GibbsSampler sampler(&lattice, prec_matrix, &engine, true, config["omega"]);
 
   using Vector = Eigen::SparseVector<double>;
   auto res = Vector(lattice.get_n_total_vertices());
@@ -48,28 +55,42 @@ int main(int argc, char *argv[]) {
   for (auto v : lattice.adj_vert)
     res.insert(v) = 0;
 
-  const std::size_t n_burnin = 1000;
-  const std::size_t n_samples = 10000;
+  const std::size_t n_burnin = config["n_burnin"];
+  const std::size_t n_samples = config["n_samples"];
 
   res = sampler.sample(res, n_burnin);
   sampler.reset_mean();
 
-  const auto start = std::chrono::high_resolution_clock::now();
-  res = sampler.sample(res, n_samples);
-  const auto end = std::chrono::high_resolution_clock::now();
-  const auto elapsed = end - start;
+  for (std::size_t n = 0; n < n_samples; ++n) {
+    res = sampler.sample(res, 1);
 
-  double local_norm = sampler.get_mean().squaredNorm();
+    double local_norm = sampler.get_mean().squaredNorm();
 
-  double norm = 0;
-  MPI_Reduce(&local_norm, &norm, 1, MPI_DOUBLE, MPI_SUM,
-             mpi_helper::debug_rank(), MPI_COMM_WORLD);
-  norm = std::sqrt(norm);
+    double norm = 0;
+    MPI_Reduce(&local_norm, &norm, 1, MPI_DOUBLE, MPI_SUM,
+               mpi_helper::debug_rank(), MPI_COMM_WORLD);
+    norm = std::sqrt(norm);
 
-  if (mpi_helper::is_debug_rank()) {
-    std::cout << "Norm = " << norm << "\n";
-    std::cout << "Time: "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed)
-              << "\n";
+    std::cout << norm << "\n";
   }
+
+  // const auto start = std::chrono::high_resolution_clock::now();
+  // res = sampler.sample(res, n_samples);
+  // const auto end = std::chrono::high_resolution_clock::now();
+  // const auto elapsed = end - start;
+
+  // double local_norm = sampler.get_mean().squaredNorm();
+
+  // double norm = 0;
+  // MPI_Reduce(&local_norm, &norm, 1, MPI_DOUBLE, MPI_SUM,
+  //            mpi_helper::debug_rank(), MPI_COMM_WORLD);
+  // norm = std::sqrt(norm);
+
+  // if (mpi_helper::is_debug_rank()) {
+  //   std::cout << "Norm = " << norm << "\n";
+  //   std::cout << "Time: "
+  //             <<
+  //             std::chrono::duration_cast<std::chrono::milliseconds>(elapsed)
+  //             << "\n";
+  // }
 }
