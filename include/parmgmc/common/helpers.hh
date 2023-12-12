29fbcc86575a218913a3c83a42279a3dc9d2e68f
Nils Friess <nils.friess@gmail.com>
2023-12-12 19:57:26 +0100

    Properly log random number generation

--- include/parmgmc/common/helpers.hh
+++ include/parmgmc/common/helpers.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "parmgmc/common/petsc_helper.hh"
 #include <algorithm>
 #include <random>
 
@@ -14,13 +15,20 @@ PetscErrorCode fill_vec_rand(Vec vec, PetscInt size, Engine &engine) {
   static std::normal_distribution<PetscReal> dist;
 
   PetscFunctionBeginUser;
+
+  PetscLogEvent rng_event;
+  PetscCall(PetscHelper::get_rng_event(&rng_event));
+  PetscCall(PetscLogEventBegin(rng_event, NULL, NULL, NULL, NULL));
+
   PetscScalar *r_arr;
   PetscCall(VecGetArray(vec, &r_arr));
   std::generate_n(r_arr, size, [&]() { return dist(engine); });
   PetscCall(VecRestoreArray(vec, &r_arr));
 
   // Estimated using perf
-  PetscCall(size * PetscLogFlops(27));
+  PetscCall(PetscLogFlops(size * 27));
+
+  PetscCall(PetscLogEventEnd(rng_event, NULL, NULL, NULL, NULL));
 
   PetscFunctionReturn(PETSC_SUCCESS);
 }
--- include/parmgmc/common/petsc_helper.hh
+++ include/parmgmc/common/petsc_helper.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <petsclog.h>
 #include <petscsys.h>
 
 namespace parmgmc {
@@ -8,6 +9,26 @@ struct PetscHelper {
     PetscInitialize(argc, argv, NULL, NULL);
   }
 
+  static PetscErrorCode get_rng_event(PetscLogEvent *evt) {
+    static PetscLogEvent rng_event;
+
+    PetscFunctionBeginUser;
+    if (!rng_event_registered) {
+      PetscClassId classid;
+      PetscCall(PetscClassIdRegister("ParMGMC", &classid));
+      PetscCall(PetscLogEventRegister("RNG", classid, &rng_event));
+
+      rng_event_registered = true;
+    }
+
+    *evt = rng_event;
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
+
   ~PetscHelper() { PetscFinalize(); }
+
+private:
+  inline static bool rng_event_registered = false;
 };
 } // namespace parmgmc
