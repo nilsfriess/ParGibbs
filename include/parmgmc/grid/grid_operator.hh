113cdc30b6cf81a26ea00c300b2ea93dca2a2ff2
Nils Friess <nils.friess@gmail.com>
2024-01-14 19:48:37 +0100

    Implement multicolor Gibbs sampler

--- include/parmgmc/grid/grid_operator.hh
+++ include/parmgmc/grid/grid_operator.hh
@@ -95,6 +95,8 @@ private:
 };
 
 struct GridOperator {
+  enum COLORS : ISColoringValue { RED, BLACK };
+
   /* Constructs a GridOperator instance for a 2d structured grid of size
    * global_x*global_y and a matrix representing an operator defined on that
    * grid. The parameter mat_assembler must be a function with signature `void
@@ -108,41 +110,64 @@ struct GridOperator {
       : global_x{global_x}, global_y{global_y},
         meshwidth_x{(upper_right.x - lower_left.x) / (global_x - 1)},
         meshwidth_y{(upper_right.y - lower_left.y) / (global_y - 1)} {
-    auto call = [&](auto err) { PetscCallAbort(MPI_COMM_WORLD, err); };
-
     const PetscInt dof_per_node = 1;
     const PetscInt stencil_width = 1;
 
     PetscFunctionBeginUser;
-    call(DMDACreate2d(PETSC_COMM_WORLD,
-                      DM_BOUNDARY_NONE,
-                      DM_BOUNDARY_NONE,
-                      DMDA_STENCIL_STAR,
-                      global_x,
-                      global_y,
-                      PETSC_DECIDE,
-                      PETSC_DECIDE,
-                      dof_per_node,
-                      stencil_width,
-                      NULL,
-                      NULL,
-                      &dm));
-    call(DMSetUp(dm));
-    call(DMDASetUniformCoordinates(
+    PetscCallVoid(DMDACreate2d(PETSC_COMM_WORLD,
+                               DM_BOUNDARY_NONE,
+                               DM_BOUNDARY_NONE,
+                               DMDA_STENCIL_STAR,
+                               global_x,
+                               global_y,
+                               PETSC_DECIDE,
+                               PETSC_DECIDE,
+                               dof_per_node,
+                               stencil_width,
+                               NULL,
+                               NULL,
+                               &dm));
+    PetscCallVoid(DMSetUp(dm));
+    PetscCallVoid(DMDASetUniformCoordinates(
         dm, lower_left.x, upper_right.x, lower_left.y, upper_right.y, 0, 0));
 
     // Allocate memory for matrix and initialise non-zero pattern
-    call(DMCreateMatrix(dm, &mat));
+    PetscCallVoid(DMCreateMatrix(dm, &mat));
 
     // Call provided assembly functor to fill matrix
-    call(mat_assembler(mat, dm));
+    PetscCallVoid(mat_assembler(mat, dm));
+
+    // Create red/black coloring
+    PetscCallVoid(setup_coloring());
+
+    MatType type;
+    PetscCallVoid(MatGetType(mat, &type));
+    if (std::strcmp(type, MATMPIAIJ) == 0) {
+      PetscCallVoid(create_rb_scatter());
+    }
 
     PetscFunctionReturnVoid();
   }
 
   ~GridOperator() {
-    MatDestroy(&mat);
-    DMDestroy(&dm);
+    PetscFunctionBeginUser;
+    PetscCallVoid(MatDestroy(&mat));
+    PetscCallVoid(DMDestroy(&dm));
+    PetscCallVoid(ISColoringDestroy(&coloring));
+
+    if (sct_vec)
+      PetscCallVoid(VecDestroy(&sct_vec));
+
+    // if (scatter_red)
+    //   PetscCallVoid(VecScatterDestroy(&scatter_red));
+
+    // if (scatter_black)
+    //   PetscCallVoid(VecScatterDestroy(&scatter_black));
+
+    if (scatter)
+      PetscCallVoid(VecScatterDestroy(&scatter));
+
+    PetscFunctionReturnVoid();
   }
 
   void set_lowrank_factor(Mat A, Vec S, Vec S_chol) {
@@ -155,10 +180,11 @@ struct GridOperator {
     PetscCall(MatMult(mat, xin, xout));
 
     if (lowrank_update) {
-      Vec tmp;
+      Vec tmp; // TODO: Make this a class member to avoid reallocations
       PetscCall(VecDuplicate(xout, &tmp));
       PetscCall(lowrank_update->apply(xin, tmp));
       PetscCall(VecAXPY(xout, 1., tmp));
+      PetscCall(VecDestroy(&tmp));
     }
 
     PetscFunctionReturn(PETSC_SUCCESS);
@@ -173,6 +199,189 @@ struct GridOperator {
   DM dm;
   Mat mat;
 
+  // VecScatter scatter_black = nullptr;
+  // VecScatter scatter_red = nullptr;
+  VecScatter scatter = nullptr;
+
+  ISColoring coloring; // red-black coloring
+
   std::unique_ptr<LowrankUpdate<Vec>> lowrank_update;
+
+  Vec sct_vec = nullptr;
+
+private:
+  PetscErrorCode setup_coloring() {
+    PetscFunctionBeginUser;
+
+    const PetscInt ncolors = 2;
+
+    PetscInt nnodes;
+    PetscCall(MatGetLocalSize(mat, &nnodes, NULL));
+
+    ISLocalToGlobalMapping ltog;
+    PetscCall(DMGetLocalToGlobalMapping(dm, &ltog));
+
+    std::vector<ISColoringValue> colors(nnodes);
+    for (PetscInt i = 0; i < nnodes; ++i) {
+      PetscInt global_idx;
+      PetscCall(ISLocalToGlobalMappingApply(ltog, 1, &i, &global_idx));
+      if (global_idx % 2 == 0)
+        colors[i] = COLORS::RED;
+      else
+        colors[i] = COLORS::BLACK;
+    }
+
+    PetscCall(ISColoringCreate(MPI_COMM_WORLD,
+                               ncolors,
+                               nnodes,
+                               colors.data(),
+                               PETSC_COPY_VALUES,
+                               &coloring));
+    PetscCall(ISColoringSetType(coloring, IS_COLORING_LOCAL));
+    PetscCall(ISColoringViewFromOptions(coloring, NULL, "-rb_coloring_view"));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
+
+  PetscErrorCode create_rb_scatter() {
+    PetscFunctionBeginUser;
+
+    Vec global_vec;
+    PetscCall(DMGetGlobalVector(dm, &global_vec));
+
+    PetscInt first_row;
+    PetscCall(MatGetOwnershipRange(mat, &first_row, NULL));
+
+    Mat B;
+    const PetscInt *colmap;
+    PetscCall(MatMPIAIJGetSeqAIJ(mat, NULL, &B, &colmap));
+
+    const PetscInt *Bi, *Bj;
+    PetscCall(MatSeqAIJGetCSRAndMemType(B, &Bi, &Bj, NULL, NULL));
+    PetscInt Brows;
+    PetscCall(MatGetLocalSize(B, &Brows, NULL));
+    PetscInt Acols;
+    PetscCall(MatGetSize(mat, NULL, &Acols));
+
+    /* This array will have non-zero values at indices corresponding to ghost
+       vertices. These are identified by looping over the rows of the
+       off-diagonal portion B of the given matrix in MPIAIJ format and marking
+       columns that contain non-zero entries.
+
+       For example a 5x5 DMDA grid on four processors would lead to an indices
+       array (on rank 0) that could look like this:
+       indices = {0 0 0 0 0 0 0 0 0 2 0 5 0 8 0 6 7 8 0 0 0 0 0 0 0}.
+
+       The values of the non-zero entries are the rows where B contains non-zero
+       columns, using global indexing. These are used below to determine which
+       ghost values have to be communicated during red Gibbs sweeps and which
+       during black sweeps.
+     */
+    std::vector<PetscInt> indices(Acols, 0);
+    std::size_t nz_cols = 0;
+    for (PetscInt row = 0; row < Brows; ++row) {
+      for (PetscInt k = Bi[row]; k < Bi[row + 1]; ++k) {
+        /* We have to use colmap here since B is compactified, i.e., its
+         * non-zero columns are {0, ..., nz_cols} (see MatSetUpMultiply_MPIAIJ).
+         */
+        if (!indices[colmap[Bj[k]]])
+          nz_cols++;
+        indices[colmap[Bj[k]]] = first_row + row;
+      }
+    }
+
+    // Form array of needed columns.
+    std::vector<PetscInt> ghost_arr(nz_cols);
+    PetscInt cnt = 0;
+    for (PetscInt i = 0; i < Acols; ++i)
+      if (indices[i])
+        ghost_arr[cnt++] = i;
+
+    // PetscCall(PetscIntView(
+    //     ghost_arr.size(), ghost_arr.data(), PETSC_VIEWER_STDOUT_WORLD));
+
+    IS from, to;
+    PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+                              ghost_arr.size(),
+                              ghost_arr.data(),
+                              PETSC_COPY_VALUES,
+                              &from));
+    PetscCall(ISCreateStride(MPI_COMM_WORLD, ghost_arr.size(), 0, 1, &to));
+
+    PetscCall(MatCreateVecs(B, &sct_vec, NULL));
+    PetscCall(VecScatterCreate(global_vec, from, sct_vec, to, &scatter));
+    PetscCall(
+        VecScatterViewFromOptions(scatter, NULL, "-ghost_vecscatter_view"));
+
+    PetscCall(ISDestroy(&from));
+    PetscCall(ISDestroy(&to));
+
+    // // Split ghost indices into red and black
+    // std::vector<PetscInt> black_ghosts;
+    // std::vector<PetscInt> red_ghosts;
+    // std::vector<PetscInt> black_target;
+    // std::vector<PetscInt> red_target;
+    // black_ghosts.reserve((ghost_arr.size() + 1) / 2);
+    // red_ghosts.reserve((ghost_arr.size() + 1) / 2);
+    // for (std::size_t i = 0; i < nz_cols; ++i) {
+    //   const auto ghost = ghost_arr[i];
+
+    //   /* If the "origin index" is red, then the ghost is black. */
+    //   if (indices[ghost] % 2 == 0) {
+    //     black_ghosts.push_back(ghost);
+    //     black_target.push_back(i);
+    //   } else {
+    //     red_ghosts.push_back(ghost);
+    //     red_target.push_back(i);
+    //   }
+    // }
+
+    // IS from_black, from_red;
+    // PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+    //                           black_ghosts.size(),
+    //                           black_ghosts.data(),
+    //                           PETSC_COPY_VALUES,
+    //                           &from_black));
+    // PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+    //                           red_ghosts.size(),
+    //                           red_ghosts.data(),
+    //                           PETSC_COPY_VALUES,
+    //                           &from_red));
+
+    // IS to_red, to_black;
+    // PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+    //                           black_target.size(),
+    //                           black_target.data(),
+    //                           PETSC_COPY_VALUES,
+    //                           &to_black));
+    // PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+    //                           red_target.size(),
+    //                           red_target.data(),
+    //                           PETSC_COPY_VALUES,
+    //                           &to_red));
+
+    // Vec sct_vec;
+    // PetscCall(MatCreateVecs(B, &sct_vec, NULL));
+
+    // PetscCall(VecScatterCreate(
+    //     global_vec, from_black, sct_vec, to_black, &scatter_black));
+    // PetscCall(VecScatterViewFromOptions(
+    //     scatter_black, NULL, "-ghost_vecscatter_view"));
+
+    // PetscCall(
+    //     VecScatterCreate(global_vec, from_red, sct_vec, to_red,
+    //     &scatter_red));
+    // PetscCall(
+    //     VecScatterViewFromOptions(scatter_red, NULL,
+    //     "-ghost_vecscatter_view"));
+
+    // PetscCall(ISDestroy(&to_red));
+    // PetscCall(ISDestroy(&to_black));
+    // PetscCall(ISDestroy(&from_red));
+    // PetscCall(ISDestroy(&from_black));
+    PetscCall(DMRestoreGlobalVector(dm, &global_vec));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
 };
 } // namespace parmgmc
---
+++ include/parmgmc/samplers/gibbs.hh
@@ -0,0 +1,370 @@
+#pragma once
+
+#include "parmgmc/common/helpers.hh"
+#include "parmgmc/grid/grid_operator.hh"
+
+#include <cassert>
+#include <cstring>
+#include <memory>
+
+#include <mpi.h>
+#include <petscdm.h>
+#include <petscerror.h>
+#include <petscis.h>
+#include <petscistypes.h>
+#include <petscmat.h>
+#include <petscsftypes.h>
+#include <petscsystypes.h>
+#include <petscvec.h>
+#include <petscviewer.h>
+
+namespace parmgmc {
+
+template <class Engine> class GibbsSampler {
+public:
+  GibbsSampler(std::shared_ptr<GridOperator> grid_operator, Engine *engine,
+               PetscReal omega = 1.)
+      : grid_operator{grid_operator}, engine{engine}, omega{omega} {
+    PetscFunctionBeginUser;
+
+    PetscCallVoid(MatCreateVecs(grid_operator->mat, &rand_vec, NULL));
+    PetscCallVoid(VecGetLocalSize(rand_vec, &rand_vec_size));
+
+    // Diagonal
+    PetscCallVoid(MatCreateVecs(grid_operator->mat, &sqrt_diag, NULL));
+    PetscCallVoid(MatGetDiagonal(grid_operator->mat, sqrt_diag));
+    PetscCallVoid(VecSqrtAbs(sqrt_diag));
+
+    // Inverse diagonal
+    PetscCallVoid(MatCreateVecs(grid_operator->mat, &inv_diag, NULL));
+    PetscCallVoid(MatGetDiagonal(grid_operator->mat, inv_diag));
+    PetscCallVoid(VecReciprocal(inv_diag));
+
+    // Square root of inverse diagonal
+    PetscCallVoid(VecDuplicate(inv_diag, &inv_sqrt_diag));
+    PetscCallVoid(VecCopy(inv_diag, inv_sqrt_diag));
+    PetscCallVoid(VecSqrtAbs(inv_sqrt_diag));
+
+    // sqrt((2-w)*w) * square root of inverse diagonal
+    PetscCallVoid(VecDuplicate(inv_diag, &inv_sqrt_diag_omega));
+    PetscCallVoid(VecCopy(inv_sqrt_diag, inv_sqrt_diag_omega));
+    PetscCallVoid(
+        VecScale(inv_sqrt_diag_omega, std::sqrt((2 - omega) * omega)));
+
+    MatType type;
+    PetscCallVoid(MatGetType(grid_operator->mat, &type));
+
+    Mat Ad = nullptr;
+    if (std::strcmp(type, MATMPIAIJ) == 0) {
+      PetscCallVoid(MatMPIAIJGetSeqAIJ(grid_operator->mat, &Ad, NULL, NULL));
+    } else if (std::strcmp(type, MATSEQAIJ) == 0) {
+      Ad = grid_operator->mat;
+    } else {
+      PetscCheckAbort(false,
+                      MPI_COMM_WORLD,
+                      PETSC_ERR_SUP,
+                      "Only MATMPIAIJ and MATSEQAIJ types are supported");
+    }
+
+    const PetscInt *i, *j;
+    PetscReal *a;
+
+    PetscCallVoid(MatSeqAIJGetCSRAndMemType(Ad, &i, &j, &a, NULL));
+
+    PetscInt rows;
+    PetscCallVoid(MatGetSize(Ad, &rows, NULL));
+
+    diag_ptrs.reserve(rows);
+    for (PetscInt row = 0; row < rows; ++row) {
+      const auto row_start = i[row];
+      const auto row_end = i[row + 1];
+
+      for (PetscInt k = row_start; k < row_end; ++k) {
+        const auto col = j[k];
+        if (col == row)
+          diag_ptrs.push_back(k);
+      }
+    }
+    PetscCheckAbort(diag_ptrs.size() == (std::size_t)rows,
+                    MPI_COMM_WORLD,
+                    PETSC_ERR_SUP,
+                    "Diagonal elements of precision matrix cannot be zero");
+
+    PetscCallVoid(PetscLogEventRegister("MAT_SOR_RB", 123, &MAT_SOR_RB));
+
+    PetscFunctionReturnVoid();
+  }
+
+  PetscErrorCode sample(Vec sample, [[maybe_unused]] Vec rhs,
+                        [[maybe_unused]] std::size_t n_samples = 1) {
+    PetscFunctionBeginUser;
+
+    // PetscCall(VecAXPY(rand_vec, 1., rhs));
+
+    MatType type;
+    PetscCall(MatGetType(grid_operator->mat, &type));
+
+    if (std::strcmp(type, MATMPIAIJ) == 0) {
+      if (!ghost_vec) {
+        Mat Ao;
+        PetscCall(MatMPIAIJGetSeqAIJ(grid_operator->mat, NULL, &Ao, NULL));
+        PetscCall(MatCreateVecs(Ao, &ghost_vec, NULL));
+      }
+
+      PetscCall(gibbs_rb_mpi(sample, rhs));
+    } else if (std::strcmp(type, MATSEQAIJ) == 0) {
+      PetscCall(gibbs_rb_seq(sample, rhs));
+    } else {
+      PetscCheck(false,
+                 MPI_COMM_WORLD,
+                 PETSC_ERR_SUP,
+                 "Only MATMPIAIJ and MATSEQAIJ types are supported");
+    }
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
+
+  ~GibbsSampler() {
+    VecDestroy(&rand_vec);
+    VecDestroy(&inv_sqrt_diag_omega);
+    VecDestroy(&inv_sqrt_diag);
+    VecDestroy(&inv_diag);
+    VecDestroy(&ghost_vec);
+    VecDestroy(&sqrt_diag);
+  }
+
+private:
+  // TODO: Add FLOP logging
+  PetscErrorCode gibbs_rb_seq(Vec sample, Vec rhs) {
+    PetscFunctionBeginUser;
+
+    PetscCall(fill_vec_rand(rand_vec, rand_vec_size, *engine));
+    PetscCall(VecPointwiseMult(rand_vec, rand_vec, inv_sqrt_diag_omega));
+    PetscCall(VecAXPY(rand_vec, 1., rhs));
+
+    PetscReal *sample_arr;
+    const PetscReal *rand_arr;
+
+    PetscCall(VecGetArray(sample, &sample_arr));
+    PetscCall(VecGetArrayRead(rand_vec, &rand_arr));
+
+    const PetscInt *rowptr, *colptr;
+    PetscReal *matvals;
+
+    PetscCall(MatSeqAIJGetCSRAndMemType(
+        grid_operator->mat, &rowptr, &colptr, &matvals, NULL));
+
+    PetscInt rows;
+    PetscCall(MatGetSize(grid_operator->mat, &rows, NULL));
+
+    const PetscScalar *inv_diag_arr;
+    PetscCall(VecGetArrayRead(inv_diag, &inv_diag_arr));
+
+    const auto gibbs_kernel = [&](PetscInt row) {
+      const auto row_start = rowptr[row];
+      const auto row_end = rowptr[row + 1];
+
+      PetscReal sum = 0.;
+
+      // Lower triangular part
+      const auto n_below = diag_ptrs[row] - row_start;
+      for (PetscInt k = 0; k < n_below; ++k)
+        sum -= matvals[row_start + k] * sample_arr[colptr[row_start + k]];
+
+      // Upper triangular part
+      const auto n_above = row_end - diag_ptrs[row] - 1;
+      for (PetscInt k = 0; k < n_above; ++k)
+        sum -= matvals[diag_ptrs[row] + 1 + k] *
+               sample_arr[colptr[diag_ptrs[row] + 1 + k]];
+
+      // Update sample
+      sample_arr[row] = (1 - omega) * sample_arr[row] + rand_arr[row] +
+                        omega * inv_diag_arr[row] * sum;
+    };
+
+    IS *is_colorings;
+    PetscInt n_colors;
+    PetscCall(ISColoringGetIS(
+        grid_operator->coloring, PETSC_USE_POINTER, &n_colors, &is_colorings));
+
+    for (PetscInt color = 0; color < n_colors; ++color) {
+      PetscInt n_indices;
+      PetscCall(ISGetLocalSize(is_colorings[color], &n_indices));
+
+      const PetscInt *indices;
+      PetscCall(ISGetIndices(is_colorings[color], &indices));
+
+      for (PetscInt i = 0; i < n_indices; ++i)
+        gibbs_kernel(indices[i]);
+
+      PetscCall(ISRestoreIndices(is_colorings[color], &indices));
+    }
+
+    PetscCall(ISColoringRestoreIS(
+        grid_operator->coloring, PETSC_USE_POINTER, &is_colorings));
+
+    PetscCall(VecRestoreArrayRead(inv_diag, &inv_diag_arr));
+    PetscCall(VecRestoreArray(sample, &sample_arr));
+    PetscCall(VecRestoreArrayRead(rhs, &rand_arr));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
+
+  PetscErrorCode gibbs_rb_mpi(Vec sample, Vec rhs) {
+    PetscFunctionBeginUser;
+
+    ISLocalToGlobalMapping ltog;
+    PetscCall(DMGetLocalToGlobalMapping(grid_operator->dm, &ltog));
+
+    PetscCall(fill_vec_rand(rand_vec, rand_vec_size, *engine));
+    PetscCall(VecPointwiseMult(rand_vec, rand_vec, inv_sqrt_diag_omega));
+    PetscCall(VecAXPY(rand_vec, 1., rhs));
+
+    Mat Ad, Ao;
+    PetscCall(MatMPIAIJGetSeqAIJ(grid_operator->mat, &Ad, &Ao, NULL));
+
+    const PetscInt *rowptr, *colptr;
+    PetscReal *matvals;
+    PetscCall(MatSeqAIJGetCSRAndMemType(Ad, &rowptr, &colptr, &matvals, NULL));
+
+    const PetscInt *B_rowptr, *B_colptr;
+    PetscReal *B_matvals;
+    PetscCall(
+        MatSeqAIJGetCSRAndMemType(Ao, &B_rowptr, &B_colptr, &B_matvals, NULL));
+
+    PetscInt rows;
+    PetscCall(MatGetSize(Ad, &rows, NULL));
+
+    PetscReal *sample_arr;
+    const PetscReal *rand_arr, *inv_diag_arr, *ghost_arr;
+
+    PetscCall(VecGetArrayRead(inv_diag, &inv_diag_arr));
+    PetscCall(VecGetArrayRead(rand_vec, &rand_arr));
+
+    const auto gibbs_kernel = [&](PetscInt row) {
+      const auto row_start = rowptr[row];
+      const auto row_end = rowptr[row + 1];
+
+      PetscReal sum = 0.;
+
+      // Lower triangular part
+      const auto n_below = diag_ptrs[row] - row_start;
+      for (PetscInt k = 0; k < n_below; ++k)
+        sum -= matvals[row_start + k] * sample_arr[colptr[row_start + k]];
+
+      // Upper triangular part
+      const auto n_above = row_end - diag_ptrs[row] - 1;
+      for (PetscInt k = 0; k < n_above; ++k)
+        sum -= matvals[diag_ptrs[row] + 1 + k] *
+               sample_arr[colptr[diag_ptrs[row] + 1 + k]];
+
+      for (PetscInt k = B_rowptr[row]; k < B_rowptr[row + 1]; ++k)
+        sum -= B_matvals[k] * ghost_arr[B_colptr[k]];
+
+      // Update sample
+      sample_arr[row] = (1 - omega) * sample_arr[row] + rand_arr[row] +
+                        omega * inv_diag_arr[row] * sum;
+    };
+
+    IS *is_colorings;
+    PetscInt n_colors;
+    PetscCall(ISColoringGetIS(
+        grid_operator->coloring, PETSC_USE_POINTER, &n_colors, &is_colorings));
+
+    for (PetscInt color = 0; color < n_colors; ++color) {
+      PetscCall(VecZeroEntries(ghost_vec));
+      PetscCall(VecScatterBegin(grid_operator->scatter,
+                                sample,
+                                ghost_vec,
+                                INSERT_VALUES,
+                                SCATTER_FORWARD));
+
+      PetscInt n_indices;
+      PetscCall(ISGetLocalSize(is_colorings[color], &n_indices));
+
+      const PetscInt *indices;
+      PetscCall(ISGetIndices(is_colorings[color], &indices));
+
+      PetscCall(VecScatterEnd(grid_operator->scatter,
+                              sample,
+                              ghost_vec,
+                              INSERT_VALUES,
+                              SCATTER_FORWARD));
+
+      PetscCall(VecGetArray(sample, &sample_arr));
+      PetscCall(VecGetArrayRead(ghost_vec, &ghost_arr));
+
+      // PetscCall(PetscIntView(n_indices, indices, PETSC_VIEWER_STDOUT_WORLD));
+      
+      for (PetscInt i = 0; i < n_indices; ++i) {
+        gibbs_kernel(indices[i]);
+      }
+
+      PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
+      PetscCall(VecRestoreArray(sample, &sample_arr));
+      PetscCall(ISRestoreIndices(is_colorings[color], &indices));
+    }
+
+    // /// Red sweep
+
+    // for (PetscInt row = 0; row < rows; row++) {
+    //   PetscInt global_row;
+    //   PetscCall(ISLocalToGlobalMappingApply(ltog, 1, &row, &global_row));
+    //   if (global_row % 2 == 0)
+    //     gibbs_kernel(row);
+    // }
+    // PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
+    // PetscCall(VecRestoreArray(sample, &sample_arr));
+
+    // /// Black sweep
+    // PetscCall(VecZeroEntries(ghost_vec));
+    // PetscCall(VecScatterBegin(grid_operator->scatter,
+    //                           sample,
+    //                           ghost_vec,
+    //                           INSERT_VALUES,
+    //                           SCATTER_FORWARD));
+    // PetscCall(VecScatterEnd(grid_operator->scatter,
+    //                         sample,
+    //                         ghost_vec,
+    //                         INSERT_VALUES,
+    //                         SCATTER_FORWARD));
+
+    // PetscCall(VecGetArray(sample, &sample_arr));
+    // PetscCall(VecGetArrayRead(ghost_vec, &ghost_arr));
+    // for (PetscInt row = 0; row < rows; row++) {
+    //   PetscInt global_row;
+    //   PetscCall(ISLocalToGlobalMappingApply(ltog, 1, &row, &global_row));
+    //   if (global_row % 2 != 0)
+    //     gibbs_kernel(row);
+    // }
+    // PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
+    // PetscCall(VecRestoreArray(sample, &sample_arr));
+
+    PetscCall(ISColoringRestoreIS(
+        grid_operator->coloring, PETSC_USE_POINTER, &is_colorings));
+
+    PetscCall(VecRestoreArrayRead(rand_vec, &rand_arr));
+    PetscCall(VecRestoreArrayRead(inv_diag, &inv_diag_arr));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+  }
+
+  std::shared_ptr<GridOperator> grid_operator;
+  Engine *engine;
+  PetscReal omega;
+
+  Vec sqrt_diag;
+  Vec inv_sqrt_diag_omega;
+  Vec inv_diag;
+  Vec inv_sqrt_diag;
+
+  Vec rand_vec;
+  PetscInt rand_vec_size;
+
+  PetscLogEvent MAT_SOR_RB;
+
+  /// Only used in parallel execution
+  Vec ghost_vec = nullptr;
+  std::vector<PetscInt> diag_ptrs; // Indices of the diagonal entries
+};
+} // namespace parmgmc
