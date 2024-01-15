1c6f0f981bc37092050b021e057ee76811744714
Nils Friess <nils.friess@gmail.com>
2024-01-15 13:01:56 +0100

    Remove unnused vectors

--- include/parmgmc/samplers/gibbs.hh
+++ include/parmgmc/samplers/gibbs.hh
@@ -30,24 +30,15 @@ public:
     PetscCallVoid(MatCreateVecs(grid_operator->mat, &rand_vec, NULL));
     PetscCallVoid(VecGetLocalSize(rand_vec, &rand_vec_size));
 
-    // Diagonal
-    PetscCallVoid(MatCreateVecs(grid_operator->mat, &sqrt_diag, NULL));
-    PetscCallVoid(MatGetDiagonal(grid_operator->mat, sqrt_diag));
-    PetscCallVoid(VecSqrtAbs(sqrt_diag));
-
     // Inverse diagonal
     PetscCallVoid(MatCreateVecs(grid_operator->mat, &inv_diag, NULL));
     PetscCallVoid(MatGetDiagonal(grid_operator->mat, inv_diag));
     PetscCallVoid(VecReciprocal(inv_diag));
 
-    // Square root of inverse diagonal
-    PetscCallVoid(VecDuplicate(inv_diag, &inv_sqrt_diag));
-    PetscCallVoid(VecCopy(inv_diag, inv_sqrt_diag));
-    PetscCallVoid(VecSqrtAbs(inv_sqrt_diag));
-
     // sqrt((2-w)*w) * square root of inverse diagonal
     PetscCallVoid(VecDuplicate(inv_diag, &inv_sqrt_diag_omega));
-    PetscCallVoid(VecCopy(inv_sqrt_diag, inv_sqrt_diag_omega));
+    PetscCallVoid(VecCopy(inv_diag, inv_sqrt_diag_omega));
+    PetscCallVoid(VecSqrtAbs(inv_sqrt_diag_omega));
     PetscCallVoid(
         VecScale(inv_sqrt_diag_omega, std::sqrt((2 - omega) * omega)));
 
@@ -125,12 +116,14 @@ public:
   }
 
   ~GibbsSampler() {
-    VecDestroy(&rand_vec);
-    VecDestroy(&inv_sqrt_diag_omega);
-    VecDestroy(&inv_sqrt_diag);
-    VecDestroy(&inv_diag);
-    VecDestroy(&ghost_vec);
-    VecDestroy(&sqrt_diag);
+    PetscFunctionBeginUser;
+
+    PetscCallVoid(VecDestroy(&rand_vec));
+    PetscCallVoid(VecDestroy(&inv_sqrt_diag_omega));
+    PetscCallVoid(VecDestroy(&inv_diag));
+    PetscCallVoid(VecDestroy(&ghost_vec));
+
+    PetscFunctionReturnVoid();
   }
 
 private:
@@ -213,9 +206,6 @@ private:
   PetscErrorCode gibbs_rb_mpi(Vec sample, Vec rhs) {
     PetscFunctionBeginUser;
 
-    ISLocalToGlobalMapping ltog;
-    PetscCall(DMGetLocalToGlobalMapping(grid_operator->dm, &ltog));
-
     PetscCall(fill_vec_rand(rand_vec, rand_vec_size, *engine));
     PetscCall(VecPointwiseMult(rand_vec, rand_vec, inv_sqrt_diag_omega));
     PetscCall(VecAXPY(rand_vec, 1., rhs));
@@ -244,19 +234,17 @@ private:
     const auto gibbs_kernel = [&](PetscInt row) {
       const auto row_start = rowptr[row];
       const auto row_end = rowptr[row + 1];
+      const auto row_diag = diag_ptrs[row];
 
       PetscReal sum = 0.;
 
       // Lower triangular part
-      const auto n_below = diag_ptrs[row] - row_start;
-      for (PetscInt k = 0; k < n_below; ++k)
-        sum -= matvals[row_start + k] * sample_arr[colptr[row_start + k]];
+      for (PetscInt k = row_start; k < row_diag; ++k)
+        sum -= matvals[k] * sample_arr[colptr[k]];
 
       // Upper triangular part
-      const auto n_above = row_end - diag_ptrs[row] - 1;
-      for (PetscInt k = 0; k < n_above; ++k)
-        sum -= matvals[diag_ptrs[row] + 1 + k] *
-               sample_arr[colptr[diag_ptrs[row] + 1 + k]];
+      for (PetscInt k = row_diag + 1; k < row_end; ++k)
+        sum -= matvals[k] * sample_arr[colptr[k]];
 
       for (PetscInt k = B_rowptr[row]; k < B_rowptr[row + 1]; ++k)
         sum -= B_matvals[k] * ghost_arr[B_colptr[k]];
@@ -265,6 +253,9 @@ private:
       sample_arr[row] = (1 - omega) * sample_arr[row] + rand_arr[row] +
                         omega * inv_diag_arr[row] * sum;
     };
+
+    PetscInt first_row;
+    PetscCall(MatGetOwnershipRange(grid_operator->mat, &first_row, NULL));
 
     IS *is_colorings;
     PetscInt n_colors;
@@ -295,50 +286,17 @@ private:
       PetscCall(VecGetArrayRead(ghost_vec, &ghost_arr));
 
       // PetscCall(PetscIntView(n_indices, indices, PETSC_VIEWER_STDOUT_WORLD));
-      
+
       for (PetscInt i = 0; i < n_indices; ++i) {
-        gibbs_kernel(indices[i]);
+        gibbs_kernel(indices[i] - first_row);
       }
 
       PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
       PetscCall(VecRestoreArray(sample, &sample_arr));
       PetscCall(ISRestoreIndices(is_colorings[color], &indices));
+
+      // PetscCall(VecView(sample, PETSC_VIEWER_STDOUT_WORLD));
     }
-
-    // /// Red sweep
-
-    // for (PetscInt row = 0; row < rows; row++) {
-    //   PetscInt global_row;
-    //   PetscCall(ISLocalToGlobalMappingApply(ltog, 1, &row, &global_row));
-    //   if (global_row % 2 == 0)
-    //     gibbs_kernel(row);
-    // }
-    // PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
-    // PetscCall(VecRestoreArray(sample, &sample_arr));
-
-    // /// Black sweep
-    // PetscCall(VecZeroEntries(ghost_vec));
-    // PetscCall(VecScatterBegin(grid_operator->scatter,
-    //                           sample,
-    //                           ghost_vec,
-    //                           INSERT_VALUES,
-    //                           SCATTER_FORWARD));
-    // PetscCall(VecScatterEnd(grid_operator->scatter,
-    //                         sample,
-    //                         ghost_vec,
-    //                         INSERT_VALUES,
-    //                         SCATTER_FORWARD));
-
-    // PetscCall(VecGetArray(sample, &sample_arr));
-    // PetscCall(VecGetArrayRead(ghost_vec, &ghost_arr));
-    // for (PetscInt row = 0; row < rows; row++) {
-    //   PetscInt global_row;
-    //   PetscCall(ISLocalToGlobalMappingApply(ltog, 1, &row, &global_row));
-    //   if (global_row % 2 != 0)
-    //     gibbs_kernel(row);
-    // }
-    // PetscCall(VecRestoreArrayRead(ghost_vec, &ghost_arr));
-    // PetscCall(VecRestoreArray(sample, &sample_arr));
 
     PetscCall(ISColoringRestoreIS(
         grid_operator->coloring, PETSC_USE_POINTER, &is_colorings));
@@ -353,10 +311,8 @@ private:
   Engine *engine;
   PetscReal omega;
 
-  Vec sqrt_diag;
   Vec inv_sqrt_diag_omega;
   Vec inv_diag;
-  Vec inv_sqrt_diag;
 
   Vec rand_vec;
   PetscInt rand_vec_size;
