6ed429ff36bb119151b8e3ead8a3b1b3d8c8803b
Nils Friess <nils.friess@gmail.com>
2024-01-19 16:09:35 +0100

    Fix some bugs in MGMC sampler

--- include/parmgmc/samplers/mgmc.hh
+++ include/parmgmc/samplers/mgmc.hh
@@ -20,12 +20,24 @@ template <class Engine> class MultigridSampler {
 public:
   MultigridSampler(
       std::shared_ptr<GridOperator> grid_operator, Engine *engine,
-      std::size_t n_levels, std::size_t n_smooth = 2,
+      std::size_t n_levels_, std::size_t n_smooth = 2,
       MGMCCycleType cycle_type = MGMCCycleType::V,
       MGMCSmoothingType smoothing_type = MGMCSmoothingType::ForwardBackward)
-      : n_levels{n_levels}, n_smooth{n_smooth}, smoothing_type{smoothing_type},
+      : n_levels{n_levels_}, n_smooth{n_smooth}, smoothing_type{smoothing_type},
         cycles{static_cast<unsigned int>(cycle_type)} {
     PetscFunctionBeginUser;
+
+    PetscInt npoints;
+    PetscCallVoid(DMDAGetCorners(
+        grid_operator->get_dm(), NULL, NULL, NULL, &npoints, NULL, NULL));
+    if ((npoints - 1) / (1 << (n_levels - 1)) == 0) {
+      n_levels = std::floor(std::log2(npoints)) + 1;
+
+      PetscCallVoid(PetscPrintf(MPI_COMM_WORLD,
+                                "[ParMGMC] In initilisation of MGMC: too many "
+                                "levels. Using %zu instead.\n",
+                                n_levels));
+    }
 
     ops.resize(n_levels);
     ops[n_levels - 1] = grid_operator;
@@ -33,38 +45,38 @@ public:
     interpolations.resize(n_levels - 1);
 
     for (std::size_t level = n_levels - 1; level > 0; --level) {
-      ops[level - 1] = std::make_shared<GridOperator>();
+      DM coarse_dm;
       PetscCallVoid(
-          DMCoarsen(ops[level]->dm, MPI_COMM_WORLD, &ops[level - 1]->dm));
-      PetscCallVoid(DMCreateInterpolation(ops[level - 1]->dm,
-                                          ops[level]->dm,
-                                          &interpolations[level - 1],
-                                          NULL));
+          DMCoarsen(ops[level]->get_dm(), MPI_COMM_WORLD, &coarse_dm));
+
+      PetscCallVoid(DMCreateInterpolation(
+          coarse_dm, ops[level]->get_dm(), &interpolations[level - 1], NULL));
 
       // Create coarser matrix using Galerkin projection
-      PetscCallVoid(MatPtAP(ops[level]->mat,
+      Mat coarse_mat;
+      PetscCallVoid(MatPtAP(ops[level]->get_mat(),
                             interpolations[level - 1],
                             MAT_INITIAL_MATRIX,
                             PETSC_DEFAULT,
-                            &ops[level - 1]->mat));
+                            &coarse_mat));
 
-      PetscCallVoid(ops[level - 1]->color_general());
-      MatType type;
-      PetscCallVoid(MatGetType(ops[level - 1]->mat, &type));
-      if (std::strcmp(type, MATMPIAIJ) == 0) {
-        PetscCallVoid(ops[level - 1]->create_rb_scatter());
-      }
+      ops[level - 1] =
+          std::make_shared<GridOperator>(coarse_dm,
+                                         coarse_mat,
+                                         ops[level]->corners().first,
+                                         ops[level]->corners().second,
+                                         ColoringType::PETSc);
     }
 
     // Create work vectors and smoothers
-    vs.resize(n_levels);
+    bs.resize(n_levels);
     xs.resize(n_levels);
     rs.resize(n_levels);
 
     for (std::size_t level = 0; level < n_levels; ++level) {
-      PetscCallVoid(DMCreateGlobalVector(ops[level]->dm, &vs[level]));
-      PetscCallVoid(VecDuplicate(vs[level], &xs[level]));
-      PetscCallVoid(VecDuplicate(vs[level], &rs[level]));
+      PetscCallVoid(DMCreateGlobalVector(ops[level]->get_dm(), &bs[level]));
+      PetscCallVoid(VecDuplicate(bs[level], &xs[level]));
+      PetscCallVoid(VecDuplicate(bs[level], &rs[level]));
 
       smoothers.push_back(
           std::make_shared<GibbsSampler<Engine>>(ops[level], engine));
@@ -80,7 +92,12 @@ public:
   PetscErrorCode sample(Vec sample, const Vec rhs) {
     PetscFunctionBeginUser;
 
-    PetscCall(sample_impl(n_levels - 1, sample, rhs));
+    PetscCall(VecCopy(rhs, bs[n_levels - 1]));
+    PetscCall(VecCopy(sample, xs[n_levels - 1]));
+
+    PetscCall(sample_impl(n_levels - 1));
+
+    PetscCall(VecCopy(xs[n_levels - 1], sample));
 
     PetscFunctionReturn(PETSC_SUCCESS);
   }
@@ -91,7 +108,7 @@ public:
     for (auto &M : interpolations)
       PetscCallVoid(MatDestroy(&M));
 
-    for (auto &V : vs)
+    for (auto &V : bs)
       PetscCallVoid(VecDestroy(&V));
 
     for (auto &V : rs)
@@ -104,39 +121,40 @@ public:
   }
 
 private:
-  PetscErrorCode sample_impl(std::size_t level, Vec sample, const Vec rhs) {
+  PetscErrorCode sample_impl(std::size_t level) {
     PetscFunctionBeginUser;
 
     if (level > 0) {
       // Pre smooth
       if (smoothing_type != MGMCSmoothingType::Symmetric)
         smoothers[level]->setSweepType(GibbsSweepType::Forward);
-      PetscCall(smoothers[level]->sample(sample, rhs, n_smooth));
+      PetscCall(smoothers[level]->sample(xs[level], bs[level], n_smooth));
 
       // Restrict residual
-      PetscCall(MatResidual(ops[level]->mat, rhs, sample, rs[level]));
       PetscCall(
-          MatRestrict(interpolations[level - 1], rs[level], xs[level - 1]));
+          MatResidual(ops[level]->get_mat(), bs[level], xs[level], rs[level]));
+      PetscCall(
+          MatRestrict(interpolations[level - 1], rs[level], bs[level - 1]));
 
       // Recursive call to multigrid sampler
-      PetscCall(VecZeroEntries(vs[level - 1]));
+      PetscCall(VecZeroEntries(xs[level - 1]));
 
       for (std::size_t c = 0; c < cycles; ++c)
-        PetscCall(sample_impl(level - 1, vs[level - 1], xs[level - 1]));
+        PetscCall(sample_impl(level - 1));
 
       // Prolongate add result
       PetscCall(MatInterpolateAdd(
-          interpolations[level - 1], xs[level - 1], sample, sample));
+          interpolations[level - 1], xs[level - 1], xs[level], xs[level]));
 
       // Post smooth
       if (smoothing_type != MGMCSmoothingType::Symmetric)
         smoothers[level]->setSweepType(GibbsSweepType::Backward);
-      PetscCall(smoothers[level]->sample(sample, rhs, n_smooth));
+      PetscCall(smoothers[level]->sample(xs[level], bs[level], n_smooth));
     } else {
       // Coarse level
       if (smoothing_type != MGMCSmoothingType::Symmetric)
         smoothers[0]->setSweepType(GibbsSweepType::Symmetric);
-      PetscCall(smoothers[0]->sample(sample, rhs, 2 * n_smooth));
+      PetscCall(smoothers[0]->sample(xs[level], bs[level], 2 * n_smooth));
     }
 
     PetscFunctionReturn(PETSC_SUCCESS);
@@ -146,9 +164,9 @@ private:
   std::vector<std::shared_ptr<GibbsSampler<Engine>>> smoothers;
 
   std::vector<Mat> interpolations;
-  std::vector<Vec> vs; // sample vector for each level
+  std::vector<Vec> xs; // sample vector for each level
   std::vector<Vec> rs; // residual vector for each level
-  std::vector<Vec> xs; // work vector for each level
+  std::vector<Vec> bs; // rhs vector for each level
 
   std::size_t n_levels;
   std::size_t n_smooth;
