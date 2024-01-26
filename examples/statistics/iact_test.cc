dd6bdf5ba6778f7e075faac8b3fb1fb3b6b41715
Nils Friess <nils.friess@gmail.com>
2024-01-26 13:15:18 +0100

    Restructure examples

---
+++ examples/statistics/iact_test.cc
@@ -0,0 +1,186 @@
+#include "mat.hh"
+#include "parmgmc/dm_hierarchy.hh"
+#include "qoi.hh"
+
+#include "parmgmc/common/helpers.hh"
+#include "parmgmc/common/petsc_helper.hh"
+#include "parmgmc/linear_operator.hh"
+#include "parmgmc/samplers/mgmc.hh"
+#include "parmgmc/samplers/sample_chain.hh"
+
+#include <mpi.h>
+#include <pcg_random.hpp>
+
+#include <petscdm.h>
+#include <petscerror.h>
+#include <petscksp.h>
+#include <petscmat.h>
+#include <petscvec.h>
+
+#include <memory>
+#include <random>
+
+using namespace parmgmc;
+
+template <class Chain>
+inline PetscErrorCode mc_error(const std::string &name, Chain &chain,
+                               Vec sample_rhs) {
+  PetscFunctionBeginUser;
+
+  Vec initial_sample;
+  PetscCall(VecDuplicate(sample_rhs, &initial_sample));
+
+  PetscInt max_samples = 10;
+  PetscOptionsGetInt(NULL, NULL, "-max_samples", &max_samples, NULL);
+  chain.set_sample(initial_sample);
+  PetscCall(VecDestroy(&initial_sample));
+
+  PetscCall(chain.sample(sample_rhs, max_samples));
+
+  PetscCall(PetscPrintf(MPI_COMM_WORLD,
+                        "%s IACT: %zu\n",
+                        name.c_str(),
+                        chain.integrated_autocorr_time()));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+  PetscHelper helper(&argc, &argv);
+
+  PetscFunctionBeginUser;
+
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Setup DM hierarchy
+  std::shared_ptr<DMHierarchy> dm_hierarchy;
+  {
+    const PetscInt dof_per_node = 1;
+    const PetscInt stencil_width = 1;
+
+    int n_vertices = 5;
+    PetscOptionsGetInt(NULL, NULL, "-n_vertices", &n_vertices, NULL);
+
+    Coordinate lower_left{0, 0};
+    Coordinate upper_right{1, 1};
+
+    DM dm;
+    PetscCall(DMDACreate2d(PETSC_COMM_WORLD,
+                           DM_BOUNDARY_NONE,
+                           DM_BOUNDARY_NONE,
+                           DMDA_STENCIL_STAR,
+                           n_vertices,
+                           n_vertices,
+                           PETSC_DECIDE,
+                           PETSC_DECIDE,
+                           dof_per_node,
+                           stencil_width,
+                           NULL,
+                           NULL,
+                           &dm));
+
+    PetscCall(DMSetUp(dm));
+    PetscCall(DMDASetUniformCoordinates(
+        dm, lower_left.x, upper_right.x, lower_left.y, upper_right.y, 0, 0));
+
+    PetscInt n_levels = 4;
+    PetscOptionsGetInt(NULL, NULL, "-n_levels", &n_levels, NULL);
+
+    dm_hierarchy = std::make_shared<DMHierarchy>(dm, n_levels);
+    // PetscCall(dm_hierarchy->print_info());
+  }
+
+  // Setup random number generator
+  pcg32 engine;
+  {
+    int seed;
+    if (rank == 0) {
+      seed = std::random_device{}();
+      PetscOptionsGetInt(NULL, NULL, "-seed", &seed, NULL);
+    }
+
+    // Send seed to all other processes
+    MPI_Bcast(&seed, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    engine.seed(seed);
+    engine.set_stream(rank);
+  }
+
+  // RHS used in samplers
+  Vec sample_rhs;
+  PetscCall(DMCreateGlobalVector(dm_hierarchy->get_fine(), &sample_rhs));
+  // PetscCall(fill_vec_rand(sample_rhs, engine));
+
+  NormQOI qoi;
+
+  PetscInt n_smooth = 2;
+  PetscOptionsGetInt(NULL, NULL, "-n_smooth", &n_smooth, NULL);
+
+  MGMCParameters params;
+  params.n_smooth = n_smooth;
+  params.cycle_type = MGMCCycleType::V;
+  params.smoothing_type = MGMCSmoothingType::Symmetric;
+
+  // Setup Multigrid sampler (using rediscretisation on each level)
+  {
+    PetscCall(
+        PetscPrintf(MPI_COMM_WORLD,
+                    "Setting up multigrid sampler with rediscretisation..."));
+
+    using Chain = SampleChain<MultigridSampler<pcg32>, NormQOI>;
+    Chain chain(qoi, 1, sample_rhs, dm_hierarchy, assemble, &engine, params);
+
+    PetscCall(PetscPrintf(MPI_COMM_WORLD, "done.\n"));
+
+    PetscCall(mc_error("MGMC (Rediscretisation)", chain, sample_rhs));
+  }
+
+  // Setup Multigrid sampler (using Galerkin product for coarse operators)
+  {
+    PetscCall(PetscPrintf(
+        MPI_COMM_WORLD,
+        "Setting up multigrid sampler with Galerkin projection..."));
+
+    // Setup fine operator
+    Mat mat;
+    PetscCall(assemble(dm_hierarchy->get_fine(), &mat));
+    auto linear_operator = std::make_shared<LinearOperator>(mat);
+
+    using Chain = SampleChain<MultigridSampler<pcg32>, NormQOI>;
+    Chain chain(
+        qoi, 1, sample_rhs, linear_operator, dm_hierarchy, &engine, params);
+
+    PetscCall(PetscPrintf(MPI_COMM_WORLD, "done.\n"));
+
+    PetscCall(mc_error("MGMC (Galerkin)", chain, sample_rhs));
+  }
+
+  // Setup Gibbs sampler
+  {
+    PetscCall(PetscPrintf(MPI_COMM_WORLD, "Setting up Gibbs sampler..."));
+
+    // Setup fine operator
+    Mat mat;
+    PetscCall(assemble(dm_hierarchy->get_fine(), &mat));
+    auto linear_operator = std::make_shared<LinearOperator>(mat);
+
+    PetscReal omega = 1.; // SOR parameter
+    PetscOptionsGetReal(NULL, NULL, "-omega", &omega, NULL);
+
+    using Chain = SampleChain<GibbsSampler<pcg32>, NormQOI>;
+    Chain chain(qoi,
+                1,
+                sample_rhs,
+                linear_operator,
+                &engine,
+                omega,
+                GibbsSweepType::Symmetric);
+    PetscCall(PetscPrintf(MPI_COMM_WORLD, "done.\n"));
+
+    PetscCall(mc_error("Gibbs", chain, sample_rhs));
+  }
+
+  PetscCall(VecDestroy(&sample_rhs));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
--- include/parmgmc/common/helpers.hh
+++ include/parmgmc/common/helpers.hh
@@ -3,6 +3,11 @@
 #include "parmgmc/common/petsc_helper.hh"
 
 #include <algorithm>
+#include <memory>
+#include <petscis.h>
+#include <petscistypes.h>
+#include <petscmat.h>
+#include <petscviewer.h>
 #include <random>
 
 #include <mpi.h>
@@ -10,10 +15,11 @@
 #include <petsclog.h>
 #include <petscsystypes.h>
 #include <petscvec.h>
+#include <utility>
 
 namespace parmgmc {
 template <class Engine>
-PetscErrorCode fill_vec_rand(Vec vec, PetscInt size, Engine &engine) {
+inline PetscErrorCode fill_vec_rand(Vec vec, PetscInt size, Engine &engine) {
   static std::normal_distribution<PetscReal> dist;
 
   PetscFunctionBeginUser;
@@ -35,13 +41,112 @@ PetscErrorCode fill_vec_rand(Vec vec, PetscInt size, Engine &engine) {
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-template <class Engine> PetscErrorCode fill_vec_rand(Vec vec, Engine &engine) {
+template <class Engine>
+inline PetscErrorCode fill_vec_rand(Vec vec, Engine &engine) {
   PetscFunctionBeginUser;
 
   PetscInt size;
   PetscCall(VecGetLocalSize(vec, &size));
 
   PetscCall(fill_vec_rand(vec, size, engine));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+inline PetscErrorCode ISColoring_for_Mat(Mat m, ISColoring *coloring) {
+  PetscFunctionBeginUser;
+
+  MatColoring mc;
+  PetscCall(MatColoringCreate(m, &mc));
+  PetscCall(MatColoringSetDistance(mc, 1));
+  PetscCall(MatColoringSetType(mc, MATCOLORINGJP));
+  PetscCall(MatColoringApply(mc, coloring));
+  PetscCall(MatColoringDestroy(&mc));
+
+  PetscCall(ISColoringSetType(*coloring, IS_COLORING_LOCAL));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+inline PetscErrorCode VecScatter_for_Mat(Mat m, VecScatter *scatter,
+                                         Vec *sct_vec = nullptr) {
+  PetscFunctionBeginUser;
+
+  PetscInt first_row;
+  PetscCall(MatGetOwnershipRange(m, &first_row, NULL));
+
+  Mat B;
+  const PetscInt *colmap;
+  PetscCall(MatMPIAIJGetSeqAIJ(m, NULL, &B, &colmap));
+
+  const PetscInt *Bi, *Bj;
+  PetscCall(MatSeqAIJGetCSRAndMemType(B, &Bi, &Bj, NULL, NULL));
+
+  PetscInt local_rows, global_rows;
+  PetscCall(MatGetSize(m, &global_rows, NULL));
+  PetscCall(MatGetLocalSize(m, &local_rows, NULL));
+
+  /* This array will have non-zero values at indices corresponding to ghost
+     vertices. These are identified by looping over the rows of the
+     off-diagonal portion B of the given matrix in MPIAIJ format and marking
+     columns that contain non-zero entries.
+
+     For example a 5x5 DMDA grid on four processors would lead to an indices
+     array (on rank 0) that could look like this:
+     indices = {0 0 0 0 0 0 0 0 0 2 0 5 0 8 0 6 7 8 0 0 0 0 0 0 0}.
+
+     The values of the non-zero entries are the rows where B contains non-zero
+     columns, using global indexing. These are used below to determine which
+     ghost values have to be communicated during red Gibbs sweeps and which
+     during black sweeps.
+   */
+  std::vector<PetscInt> indices(global_rows, 0);
+  std::size_t nz_cols = 0;
+  for (PetscInt row = 0; row < local_rows; ++row) {
+    for (PetscInt k = Bi[row]; k < Bi[row + 1]; ++k) {
+      /* We have to use colmap here since B is compactified, i.e., its
+       * non-zero columns are {0, ..., nz_cols} (see MatSetUpMultiply_MPIAIJ).
+       */
+      if (!indices[colmap[Bj[k]]])
+        nz_cols++;
+      indices[colmap[Bj[k]]] = 1;
+    }
+  }
+
+  // Form array of needed columns.
+  std::vector<PetscInt> ghost_arr(nz_cols);
+  PetscInt cnt = 0;
+  for (PetscInt i = 0; i < global_rows; ++i)
+    if (indices[i])
+      ghost_arr[cnt++] = i;
+
+  IS from;
+  PetscCall(ISCreateGeneral(MPI_COMM_WORLD,
+                            ghost_arr.size(),
+                            ghost_arr.data(),
+                            PETSC_COPY_VALUES,
+                            &from));
+
+  bool return_sct_vec = true;
+  if (sct_vec == nullptr)
+    return_sct_vec = false;
+
+  Vec gvec, lvec;
+
+  PetscCall(MatCreateVecs(B, &lvec, NULL));
+  // Create global vec without allocating actualy memory
+  PetscCall(VecCreateMPIWithArray(
+      MPI_COMM_WORLD, 1, local_rows, global_rows, nullptr, &gvec));
+
+  PetscCall(VecScatterCreate(gvec, from, lvec, nullptr, scatter));
+
+  if (return_sct_vec)
+    sct_vec = &lvec;
+  else
+    PetscCall(VecDestroy(&lvec));
+
+  PetscCall(ISDestroy(&from));
+  PetscCall(VecDestroy(&gvec));
 
   PetscFunctionReturn(PETSC_SUCCESS);
 }
