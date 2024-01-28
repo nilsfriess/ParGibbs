e5a2a28b3675d226730b3ccdbe71f70cf405a39b
Nils Friess <nils.friess@gmail.com>
2024-01-28 13:12:54 +0100

    Remove DM from LinearOperator

--- include/parmgmc/linear_operator.hh
+++ include/parmgmc/linear_operator.hh
@@ -12,26 +12,9 @@
 namespace parmgmc {
 class LinearOperator {
 public:
-  LinearOperator(Mat mat, DM dm, bool transfer_ownership = true)
-      : mat{mat}, dm{dm}, should_delete{transfer_ownership} {
-    PetscFunctionBeginUser;
-
-    MatType type;
-    PetscCallVoid(MatGetType(mat, &type));
-
-    if (std::strcmp(type, MATMPIAIJ) == 0) {
-      PetscCallVoid(ISColoring_for_Mat(mat, &coloring));
-      PetscCallVoid(VecScatter_for_Mat(mat, &scatter));
-    }
-
-    PetscFunctionReturnVoid();
-  }
-
   LinearOperator(Mat mat, bool transfer_ownership = true)
-      : mat(mat), should_delete{transfer_ownership} {
+      : mat{mat}, should_delete{transfer_ownership} {
     PetscFunctionBeginUser;
-
-    // PetscCallVoid(PetscObjectReference((PetscObject)mat));
 
     MatType type;
     PetscCallVoid(MatGetType(mat, &type));
@@ -45,11 +28,9 @@ public:
   }
 
   Mat get_mat() const { return mat; }
-  // DM get_dm() const { return dm; }
   ISColoring get_coloring() const { return coloring; }
   VecScatter get_scatter() const { return scatter; }
 
-  bool has_dm() const { return dm != nullptr; }
   bool has_coloring() const { return coloring != nullptr; }
 
   ~LinearOperator() {
@@ -58,17 +39,14 @@ public:
     PetscCallVoid(ISColoringDestroy(&coloring));
     PetscCallVoid(VecScatterDestroy(&scatter));
 
-    if (should_delete) {
+    if (should_delete)
       PetscCallVoid(MatDestroy(&mat));
-      PetscCallVoid(DMDestroy(&dm));
-    }
 
     PetscFunctionReturnVoid();
   }
 
 private:
   Mat mat = nullptr;
-  DM dm = nullptr;
 
   // Only set in parallel execution (i.e. when type of mat == MATMPIAIJ)
   ISColoring coloring = nullptr;
