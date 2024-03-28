33af4903e68ff2725194be3a7e886d84f1f134c2
Nils Friess <nils.friess@gmail.com>
2024-03-28 09:41:41 +0100

    Fix bug in hogwild sampler

--- include/parmgmc/samplers/hogwild.hh
+++ include/parmgmc/samplers/hogwild.hh
@@ -21,60 +21,26 @@ public:
     PetscCallVoid(MatGetDiagonal(linearOperator->getMat(), sqrtDiag));
     PetscCallVoid(VecSqrtAbs(sqrtDiag));
 
-    // Inv sqrt diag
-    PetscCallVoid(VecDuplicate(sqrtDiag, &invSqrtDiag));
-    PetscCallVoid(VecCopy(sqrtDiag, invSqrtDiag));
-    PetscCallVoid(VecReciprocal(invSqrtDiag));
+    // // Inv sqrt diag
+    // PetscCallVoid(VecDuplicate(sqrtDiag, &invSqrtDiag));
+    // PetscCallVoid(VecCopy(sqrtDiag, invSqrtDiag));
+    // PetscCallVoid(VecReciprocal(invSqrtDiag));
 
     PetscFunctionReturnVoid();
-  }
-
-  PetscErrorCode fixRhs(Vec rhs) {
-    PetscFunctionBeginUser;
-
-    if (fixedRhs == nullptr)
-      PetscCall(VecDuplicate(rhs, &fixedRhs));
-
-    PetscCall(VecCopy(rhs, fixedRhs));
-    PetscCall(VecPointwiseMult(fixedRhs, fixedRhs, invSqrtDiag));
-
-    PetscFunctionReturn(PETSC_SUCCESS);
-  }
-
-  PetscErrorCode unfixRhs() {
-    if (fixedRhs == nullptr)
-      return PETSC_SUCCESS;
-
-    PetscFunctionBeginUser;
-
-    PetscCall(VecDestroy(&fixedRhs));
-    fixedRhs = nullptr;
-
-    PetscFunctionReturn(PETSC_SUCCESS);
   }
 
   PetscErrorCode sample(Vec sample, Vec rhs, std::size_t nSamples = 1) {
     PetscFunctionBeginUser;
 
-    Vec samplerRhs;
-
-    if (fixedRhs) {
-      samplerRhs = fixedRhs;
-    } else {
-      PetscCall(VecDuplicate(rhs, &samplerRhs));
-      PetscCall(VecCopy(rhs, samplerRhs));
-      PetscCall(VecPointwiseMult(samplerRhs, samplerRhs, invSqrtDiag));
-    }
+    if (nSamples == 0)
+      PetscFunctionReturn(PETSC_SUCCESS);
 
     PetscCall(fillVecRand(randVec, randVecSize, *engine));
-    PetscCall(VecAXPY(samplerRhs, 1., randVec));
-    PetscCall(VecPointwiseMult(samplerRhs, samplerRhs, sqrtDiag));
+    PetscCall(VecPointwiseMult(randVec, randVec, sqrtDiag));
+    PetscCall(VecAXPY(randVec, 1., rhs));
 
-    PetscCall(MatSOR(linearOperator->getMat(), samplerRhs, 1., SOR_LOCAL_FORWARD_SWEEP, 0.,
+    PetscCall(MatSOR(linearOperator->getMat(), randVec, 1., SOR_LOCAL_FORWARD_SWEEP, 0.,
                      nSamples, 1., sample));
-
-    if (!fixedRhs)
-      PetscCall(VecDestroy(&samplerRhs));
 
     PetscFunctionReturn(PETSC_SUCCESS);
   }
@@ -87,9 +53,7 @@ private:
   Vec randVec;
   PetscInt randVecSize;
 
-  Vec invSqrtDiag;
+  // Vec invSqrtDiag;
   Vec sqrtDiag;
-
-  Vec fixedRhs = nullptr;
 };
 } // namespace parmgmc
--- tests/hogwild.cc
+++ tests/hogwild.cc
@@ -49,10 +49,9 @@ TEST_CASE("Hogwild sampler converges to target mean", "[.][seq][mpi][hg]") {
   op->colorMatrix(dm);
 
   pm::HogwildGibbsSampler sampler(op, &engine);
-  sampler.fixRhs(rhs);
 
-  constexpr std::size_t N_BURNIN = 10'000;
-  constexpr std::size_t N_SAMPLES = 1'000'000;
+  constexpr std::size_t N_BURNIN = 1000;
+  constexpr std::size_t N_SAMPLES = 100'000;
 
   sampler.sample(sample, rhs, N_BURNIN);
 
