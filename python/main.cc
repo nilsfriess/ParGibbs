67f21bfb86452823e655c90bd9b70a6efe0f5143
Nils Friess <nils.friess@gmail.com>
2024-04-05 11:06:04 +0200

    Clean up python bindings

--- python/main.cc
+++ python/main.cc
@@ -1,4 +1,4 @@
-
+#include <petscsystypes.h>
 #include <pybind11/pybind11.h>
 #include <random>
 
@@ -11,50 +11,51 @@
 #include "petsc_caster.hh"
 
 #include <petscerror.h>
-#include <stdexcept>
 
 namespace py = pybind11;
 using namespace parmgmc;
 
 PYBIND11_MODULE(pymgmc, m) {
   using Engine = std::mt19937;
-  auto *engine = new Engine(std::random_device{}());
+  static Engine engine{std::random_device{}()};
 
-  m.def("seedEngine", [=](int seed) { engine->seed(seed); });
+  m.def("seedEngine", [&](int seed) { engine.seed(seed); });
 
-  m.def("fillVecRand", [=](Vec v) {
+  m.def("fillVecRand", [&](Vec v) {
     PetscFunctionBeginUser;
-    PetscCallVoid(fillVecRand(v, *engine));
+    PetscCallVoid(fillVecRand(v, engine));
     PetscFunctionReturnVoid();
   });
 
-  py::class_<LinearOperator, std::shared_ptr<LinearOperator>>(m, "LinearOperator")
-      .def(py::init([](Mat m) { return std::make_shared<LinearOperator>(m, false); }))
+  py::class_<LinearOperator>(m, "LinearOperator")
+      .def(py::init([](Mat m) {
+        return LinearOperator{m, false};
+      }))
       .def("colorMatrix", py::overload_cast<>(&LinearOperator::colorMatrix),
            "Generate a colouring for the matrix stored in the operator")
       .def("colorMatrix", py::overload_cast<DM>(&LinearOperator::colorMatrix),
            "Generate a red/black colouring for the matrix stored in the operator");
 
   using GibbsSampler = MulticolorGibbsSampler<Engine>;
-  py::class_<GibbsSampler, std::shared_ptr<GibbsSampler>>(m, "GibbsSampler")
-      .def(py::init([=](const std::shared_ptr<LinearOperator> &op) {
-        return std::make_shared<GibbsSampler>(op, engine);
+  py::class_<GibbsSampler>(m, "GibbsSampler")
+      .def(py::init([&](LinearOperator &op) {
+        return GibbsSampler{op, engine};
       }))
-      .def("sample", [](const std::shared_ptr<GibbsSampler> &self, Vec rhs, Vec sample) {
+      .def("sample", [](GibbsSampler &self, Vec rhs, Vec sample) {
         PetscFunctionBeginUser;
 
-        PetscCallVoid(self->sample(sample, rhs));
+        PetscCallVoid(self.sample(sample, rhs));
 
         PetscFunctionReturnVoid();
       });
 
-  py::class_<DMHierarchy, std::shared_ptr<DMHierarchy>>(m, "DMHierarchy")
-      .def(py::init([](DM coarseSpace, std::size_t nLevels) {
-        return std::make_shared<DMHierarchy>(coarseSpace, nLevels, false);
+  py::class_<DMHierarchy>(m, "DMHierarchy")
+      .def(py::init([](DM coarseSpace, PetscInt nLevels) {
+        return DMHierarchy{coarseSpace, nLevels, false};
       }))
       .def("getDM", &DMHierarchy::getDm)
       .def("getCoarse", &DMHierarchy::getCoarse)
-      .def("getFine", &DMHierarchy::getFine, py::return_value_policy::reference)
+      .def("getFine", &DMHierarchy::getFine, py::return_value_policy::automatic_reference)
       .def("numLevels", &DMHierarchy::numLevels);
 
   py::enum_<MGMCSmoothingType>(m, "SmoothingType")
@@ -66,9 +67,8 @@ PYBIND11_MODULE(pymgmc, m) {
       .value("W", MGMCCycleType::W);
 
   using MGMCSampler = MultigridSampler<Engine>;
-  py::class_<MGMCSampler, std::shared_ptr<MGMCSampler>>(m, "MGMCSampler")
-      .def(py::init([=](const std::shared_ptr<LinearOperator> &fineOperator,
-                        const std::shared_ptr<DMHierarchy> &dmHierarchy,
+  py::class_<MGMCSampler>(m, "MGMCSampler")
+      .def(py::init([&](LinearOperator &fineOperator, const DMHierarchy &dmHierarchy,
                         MGMCSmoothingType smoothingType, MGMCCycleType cycleType,
                         std::size_t smoothingSteps) {
              MGMCParameters params;
@@ -76,28 +76,28 @@ PYBIND11_MODULE(pymgmc, m) {
              params.cycleType = cycleType;
              params.nSmooth = smoothingSteps;
 
-             return std::make_shared<MGMCSampler>(fineOperator, dmHierarchy, engine, params);
+             return MGMCSampler{fineOperator, dmHierarchy, engine, params};
            }),
            py::arg("fineOperator"), py::arg("dmHierarchy"), py::kw_only(),
            py::arg("smoothing") = MGMCSmoothingType::ForwardBackward,
            py::arg("cycle") = MGMCCycleType::V, py::arg("smoothingSteps") = 2)
-      .def("sample", [](const std::shared_ptr<MGMCSampler> &self, Vec rhs, Vec sample) {
+      .def("sample", [](MGMCSampler &self, Vec rhs, Vec sample) {
         PetscFunctionBeginUser;
 
-        PetscCallVoid(self->sample(sample, rhs));
+        PetscCallVoid(self.sample(sample, rhs));
 
         PetscFunctionReturnVoid();
       });
 
   using HogwildSampler = HogwildGibbsSampler<Engine>;
-  py::class_<HogwildSampler, std::shared_ptr<HogwildSampler>>(m, "HogwildSampler")
-      .def(py::init([=](const std::shared_ptr<LinearOperator> &op) {
-        return std::make_shared<HogwildSampler>(op, engine);
+  py::class_<HogwildSampler>(m, "HogwildSampler")
+      .def(py::init([&](const LinearOperator &op) {
+        return HogwildSampler{op, engine};
       }))
-      .def("sample", [](const std::shared_ptr<HogwildSampler> &self, Vec rhs, Vec sample) {
+      .def("sample", [](HogwildSampler &self, Vec rhs, Vec sample) {
         PetscFunctionBeginUser;
 
-        PetscCallVoid(self->sample(sample, rhs));
+        PetscCallVoid(self.sample(sample, rhs));
 
         PetscFunctionReturnVoid();
       });
