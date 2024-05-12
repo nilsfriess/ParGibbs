79fd162be8e9a763c7f2477da8a4fbbc47fdb46e
Nils Friess <nils.friess@gmail.com>
2024-05-12 12:32:39 +0200

    Use PETSc cholesky factorisation in case MKL is not available

--- include/parmgmc/samplers/pc_cholsampler.hh
+++ include/parmgmc/samplers/pc_cholsampler.hh
@@ -1,15 +1,18 @@
 #pragma once
 
+#include <petsc/private/pcimpl.h>
+#include <petscis.h>
+#include <petscmat.h>
+#include <petscpc.h>
+#include <petscsys.h>
+#include <random>
+
 #if PETSC_HAVE_MKL_CPARDISO && PETSC_HAVE_MKL_PARDISO
 
-#include "parmgmc/linear_operator.hh"
 #include "parmgmc/samplers/cholesky.hh"
 
+#include "parmgmc/linear_operator.hh"
 #include <memory>
-
-#include <petsc/private/pcimpl.h>
-#include <petscmat.h>
-#include <petscsys.h>
 
 namespace parmgmc {
 
@@ -52,6 +55,91 @@ template <class Engine = std::mt19937> PetscErrorCode PCApply_CholeskySampler(PC
   }
 
   PetscCall(pcdata->sampler->sample(x, b));
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+template <class Engine = std::mt19937> PetscErrorCode PCDestroy_CholeskySampler(PC pc) {
+  PetscFunctionBeginUser;
+
+  auto *pcdata = (PCCholeskySampler<Engine> *)pc->data;
+  if (pcdata->deleteEngine) {
+    Engine *engine = nullptr;
+    PetscCall(PCGetApplicationContext(pc, &engine));
+    delete engine;
+  }
+
+  delete pcdata;
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+template <class Engine = std::mt19937> PetscErrorCode PCCreate_CholeskySampler(PC pc) {
+  PetscFunctionBeginUser;
+
+  auto *pcchol = new PCCholeskySampler<Engine>;
+
+  pc->ops->apply = PCApply_CholeskySampler<Engine>;
+  pc->ops->destroy = PCDestroy_CholeskySampler<Engine>;
+
+  pc->data = (void *)pcchol;
+
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+}; // namespace parmgmc
+
+#else
+namespace parmgmc {
+
+template <class Engine = std::mt19937> struct PCCholeskySampler {
+  Vec r = nullptr;
+  Vec v = nullptr;
+
+  bool deleteEngine = false;
+  Engine *engine;
+
+  Mat factor;
+};
+
+template <class Engine = std::mt19937> PetscErrorCode PCApply_CholeskySampler(PC pc, Vec b, Vec x) {
+  PetscFunctionBeginUser;
+
+  auto *pcdata = (PCCholeskySampler<Engine> *)pc->data;
+  if (pcdata->v == nullptr) {
+    Mat mat;
+    PetscCall(PCGetOperators(pc, &mat, nullptr));
+    PetscCall(MatGetFactor(mat, MATSOLVERPETSC, MAT_FACTOR_CHOLESKY, &pcdata->factor));
+
+    IS rowperm, colperm;
+    PetscCall(MatGetOrdering(mat, MATORDERINGNATURAL, &rowperm, &colperm));
+
+    MatFactorInfo info;
+
+    PetscCall(MatCholeskyFactorSymbolic(pcdata->factor, mat, rowperm, &info));
+    PetscCall(MatCholeskyFactorNumeric(pcdata->factor, mat, &info));
+
+    PetscCall(ISDestroy(&rowperm));
+    PetscCall(ISDestroy(&colperm));
+
+    PetscCall(VecDuplicate(b, &pcdata->v));
+    PetscCall(VecDuplicate(b, &pcdata->r));
+
+    Engine *engine;
+    PetscCall(PCGetApplicationContext(pc, &engine));
+
+    if (!engine) {
+      pcdata->engine = new Engine(std::random_device{}());
+      pcdata->deleteEngine = true;
+    }
+  }
+
+  PetscCall(fillVecRand(pcdata->r, *pcdata->engine));
+  PetscCall(MatForwardSolve(pcdata->factor, b, pcdata->v));
+
+  PetscCall(VecAXPY(pcdata->v, 1., pcdata->r));
+
+  PetscCall(MatBackwardSolve(pcdata->factor, pcdata->v, x));
 
   PetscFunctionReturn(PETSC_SUCCESS);
 }
