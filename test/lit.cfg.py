a8181a1d0c76fa5e4bd9c6337603de32afd8eb5a
Nils Friess <nils.friess@gmail.com>
2024-06-04 15:09:03 +0200

    Simplify running tests in parallel

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -4,10 +4,16 @@ import os
 config.name = 'ParMGMC'
 config.test_format = lit.formats.ShTest(True)
 
-config.suffixes = ['.cc']
+config.suffixes = ['.c']
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.my_obj_root)
 
-config.substitutions.append(('%cxx', config.parmgmc_cxx))
+config.substitutions.append(('%cc', config.parmgmc_cc))
 config.substitutions.append(('%flags', config.parmgmc_comp))
 config.substitutions.append(('%mpirun', config.parmgmc_mpirun))
+
+try:
+    NP = lit_config.params['NP']
+except KeyError as e:
+    NP = 1
+config.substitutions.append(('%NP', NP))
