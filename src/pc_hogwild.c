8d906b78774e3c4a8ff6784b06dd5c461a47e617
Nils Friess <nils.friess@gmail.com>
2024-08-29 09:26:18 +0200

    Update Hogwild Gibbs sampler to support SampleCallbacks and custom PetscRandom

--- src/pc_hogwild.c
+++ src/pc_hogwild.c
@@ -20,6 +20,11 @@
 typedef struct {
   Vec         sqrtdiag;
   PetscRandom prand;
+  PetscBool   prand_is_initial_prand;
+
+  void *cbctx;
+  PetscErrorCode (*scb)(PetscInt, Vec, void *);
+  PetscErrorCode (*del_scb)(void *);
 } *PC_Hogwild;
 
 static PetscErrorCode PCApplyRichardson_Hogwild(PC pc, Vec b, Vec y, Vec w, PetscReal rtol, PetscReal abstol, PetscReal dtol, PetscInt its, PetscBool guesszero, PetscInt *outits, PCRichardsonConvergedReason *reason)
@@ -33,24 +38,46 @@ static PetscErrorCode PCApplyRichardson_Hogwild(PC pc, Vec b, Vec y, Vec w, Pets
 
   PetscFunctionBeginUser;
   for (PetscInt it = 0; it < its; ++it) {
+    if (hw->scb) PetscCall(hw->scb(it, y, hw->cbctx));
+
     PetscCall(VecSetRandom(w, hw->prand));
     PetscCall(VecPointwiseMult(w, w, hw->sqrtdiag));
     PetscCall(VecAXPY(w, 1., b));
     PetscCall(MatSOR(pc->pmat, w, 1., SOR_LOCAL_FORWARD_SWEEP, 0., 1., 1., y));
   }
+  if (hw->scb) PetscCall(hw->scb(its, y, hw->cbctx));
 
   *outits = its;
   *reason = PCRICHARDSON_CONVERGED_ITS;
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
+static PetscErrorCode PCReset_Hogwild(PC pc)
+{
+  PC_Hogwild hw = pc->data;
+
+  PetscFunctionBeginUser;
+  PetscCall(VecDestroy(&hw->sqrtdiag));
+  if (hw->del_scb) {
+    PetscCall(hw->del_scb(hw->cbctx));
+    hw->del_scb = NULL;
+  }
+  if (hw->prand_is_initial_prand) PetscCall(PetscRandomDestroy(&hw->prand));
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
 static PetscErrorCode PCDestroy_Hogwild(PC pc)
 {
   PC_Hogwild hw = pc->data;
+
   PetscFunctionBeginUser;
   PetscCall(VecDestroy(&hw->sqrtdiag));
-  PetscCall(PetscRandomDestroy(&hw->prand));
-  PetscCall(PetscFree(pc->data));
+  if (hw->del_scb) {
+    PetscCall(hw->del_scb(hw->cbctx));
+    hw->del_scb = NULL;
+  }
+  if (hw->prand_is_initial_prand) PetscCall(PetscRandomDestroy(&hw->prand));
+  PetscCall(PetscFree(hw));
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -63,9 +90,46 @@ static PetscErrorCode PCSetUp_Hogwild(PC pc)
   PetscCall(MatGetDiagonal(pc->pmat, hw->sqrtdiag));
   PetscCall(VecSqrtAbs(hw->sqrtdiag));
 
-  // TODO: Allow user to pass own PetscRandom
   PetscCall(PetscRandomCreate(PetscObjectComm((PetscObject)pc), &hw->prand));
   PetscCall(PetscRandomSetType(hw->prand, PARMGMC_ZIGGURAT));
+  hw->prand_is_initial_prand = PETSC_TRUE; // TODO: Use PETSc's reference counting instead
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+static PetscErrorCode PCSetSampleCallback_Hogwild(PC pc, PetscErrorCode (*cb)(PetscInt, Vec, void *), void *ctx, PetscErrorCode (*deleter)(void *))
+{
+  PC_Hogwild hw = pc->data;
+
+  PetscFunctionBeginUser;
+  if (hw->del_scb) {
+    PetscCall(hw->del_scb(hw->cbctx));
+    hw->del_scb = NULL;
+  }
+  hw->scb     = cb;
+  hw->cbctx   = ctx;
+  hw->del_scb = deleter;
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+static PetscErrorCode PCHogwildGetPetscRandom(PC pc, PetscRandom *pr)
+{
+  PC_Hogwild hw = pc->data;
+
+  PetscFunctionBeginUser;
+  *pr = hw->prand;
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+static PetscErrorCode PCHogwildSetPetscRandom(PC pc, PetscRandom pr)
+{
+  PC_Hogwild hw = pc->data;
+
+  PetscFunctionBeginUser;
+  if (hw->prand_is_initial_prand) {
+    PetscCall(PetscRandomDestroy(&hw->prand));
+    hw->prand_is_initial_prand = PETSC_FALSE;
+  }
+  hw->prand = pr;
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -79,6 +143,9 @@ PetscErrorCode PCCreate_Hogwild(PC pc)
 
   pc->ops->applyrichardson = PCApplyRichardson_Hogwild;
   pc->ops->destroy         = PCDestroy_Hogwild;
+  pc->ops->reset           = PCReset_Hogwild;
   pc->ops->setup           = PCSetUp_Hogwild;
+  PetscCall(PCRegisterSetSampleCallback(pc, PCSetSampleCallback_Hogwild));
+  PetscCall(RegisterPCSetGetPetscRandom(pc, PCHogwildSetPetscRandom, PCHogwildGetPetscRandom));
   PetscFunctionReturn(PETSC_SUCCESS);
 }
